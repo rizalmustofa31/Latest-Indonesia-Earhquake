190ccadd9b7921ed53582f10dce7a500a40f8943
Rizal Mustofa <mustofarizal1994@gmail.com>
2022-02-17 21:59:15 +0700

    perubahan dari prosedural ke opp

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -1,86 +1,94 @@
 import requests
 from bs4 import BeautifulSoup
 
-description = 'To get the latest earthquake in Indonesia from BMKG.go.id'
+class gempaterkini:
+    def __init__(self):
+        self.description = 'To get the latest earthquake in Indonesia from BMKG.go.id'
+        self.result = None
+
+    def ekstrasi_data(self):
+        """
+        Tanggal: 04 Februari 2022
+        Waktu: 17:10:45 WIB
+        Magnitudo: 5.5
+        Kedalaman: 10 km
+        Lokasi: LS=7.48 LS - BT=105.92 BT
+        Pusat gempa: Pusat gempa berada di laut 71 Km Barat Daya Bayah
+        Dirasakan: Dirasakan (Skala MMI): IV Pelabuhan Ratu, III Malingping, III Bayah, III Cihara, III Panggarangan
+        :return:
+        """
+        try:
+            content = requests.get('https://bmkg.go.id')
+        except Exception:
+            return None
+
+        if content.status_code == 200:
+            soup = BeautifulSoup(content.text, 'html.parser')
+
+            result = soup.find('span', {'class': 'waktu'})
+            result = result.text.split(', ')
+            waktu = result[1]
+            tanggal = result[0]
+
+            result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+            result = result.findChildren('li')
+            i = 0
+            magnitudo = None
+            kedalaman = None
+            ls = None
+            bt = None
+            lokasi = None
+            keterangan = None
+
+            for res in result:
+                if i == 1:
+                    magnitudo = res.text
+                elif i == 2:
+                    kedalaman = res.text
+                elif i == 3:
+                    koordinat = res.text.split(' - ')
+                    ls = koordinat [0]
+                    bt = koordinat [1]
+                elif i == 4:
+                    lokasi = res.text
+                elif i == 5:
+                    keterangan = res.text
+                i = i + 1
+
+            hasil = dict()
+            hasil['tanggal'] = tanggal
+            hasil['waktu'] = waktu
+            hasil['magnitudo'] = magnitudo
+            hasil['kedalaman'] = kedalaman
+            hasil['koordinat'] = {'ls': ls, 'bt': bt }
+            hasil['lokasi'] = lokasi
+            hasil['keterangan'] = keterangan
+            self.result = hasil
+        else:
+            return None
 
 
-def ekstrasi_data():
-    """
-    Tanggal: 04 Februari 2022
-    Waktu: 17:10:45 WIB
-    Magnitudo: 5.5
-    Kedalaman: 10 km
-    Lokasi: LS=7.48 LS - BT=105.92 BT
-    Pusat gempa: Pusat gempa berada di laut 71 Km Barat Daya Bayah
-    Dirasakan: Dirasakan (Skala MMI): IV Pelabuhan Ratu, III Malingping, III Bayah, III Cihara, III Panggarangan
-    :return:
-    """
-    try:
-        content = requests.get('https://bmkg.go.id')
-    except Exception:
-        return None
+    def tampilkan_data(self):
+        if self.result is None:
+            print("Tidak bisa menemukan data gempa terkini")
+            return
 
-    if content.status_code == 200:
-        soup = BeautifulSoup(content.text, 'html.parser')
+        print('Gempa terakhir berdasarkan BMKG')
+        print(f"Tanggal {self.result['tanggal']}")
+        print(f"Waktu {self.result['waktu']}")
+        print(f"Magnitudo: {self.result['magnitudo']}")
+        print(f"Kedalaman: {self.result['kedalaman']}")
+        print(f"Koordinat: LS={self.result['koordinat']['ls']}, BT={self.result['koordinat']['bt']}")
+        print(f"Lokasi: {self.result['lokasi']}")
+        print(f"Keterangan: {self.result['keterangan']}")
 
-        result = soup.find('span', {'class': 'waktu'})
-        result = result.text.split(', ')
-        waktu = result[1]
-        tanggal = result[0]
-
-        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
-        result = result.findChildren('li')
-        i = 0
-        magnitudo = None
-        kedalaman = None
-        ls = None
-        bt = None
-        lokasi = None
-        keterangan = None
-
-        for res in result:
-            if i == 1:
-                magnitudo = res.text
-            elif i == 2:
-                kedalaman = res.text
-            elif i == 3:
-                koordinat = res.text.split(' - ')
-                ls = koordinat [0]
-                bt = koordinat [1]
-            elif i == 4:
-                lokasi = res.text
-            elif i == 5:
-                keterangan = res.text
-            i = i + 1
-
-        hasil = dict()
-        hasil['tanggal'] = tanggal
-        hasil['waktu'] = waktu
-        hasil['magnitudo'] = magnitudo
-        hasil['kedalaman'] = kedalaman
-        hasil['koordinat'] = {'ls': ls, 'bt': bt }
-        hasil['lokasi'] = lokasi
-        hasil['keterangan'] = keterangan
-        return hasil
-    else:
-        return None
-
-
-def tampilkan_data(result):
-    if result is None:
-        print("Tidak bisa menemukan data gempa terkini")
-        return
-
-    print('Gempa terakhir berdasarkan BMKG')
-    print(f"Tanggal {result['tanggal']}")
-    print(f"Waktu {result['waktu']}")
-    print(f"Magnitudo: {result['magnitudo']}")
-    print(f"Kedalaman: {result['kedalaman']}")
-    print(f"Koordinat: LS={result['koordinat']['ls']}, BT={result['koordinat']['bt']}")
-    print(f"Lokasi: {result['lokasi']}")
-    print(f"Keterangan: {result['keterangan']}")
+    def run(self):
+        self.ekstrasi_data()
+        self.tampilkan_data()
 
 if __name__ == '__main__':
-    print('Deskripsi package', description)
-    result = ekstrasi_data()
-    tampilkan_data(result)
+    gempa_di_indonesia = gempaterkini()
+    print('Deskripsi package', gempa_di_indonesia.description)
+    gempa_di_indonesia.run()
+    # gempa_di_indonesia.ekstrasi_data()
+    # gempa_di_indonesia.tampilkan_data()
